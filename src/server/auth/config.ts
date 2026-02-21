6d6054cb51db556fbe1f63e2f8c87fd8dcda7abf
Daniel Ogunluyi <daniel.ogunluyi@gmail.com>
2026-02-21 01:13:29 -0500

    Fix Auth.js 400 by using credentials provider only

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -2,7 +2,6 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import bcrypt from "bcryptjs";
 import { type DefaultSession, type NextAuthConfig } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-import DiscordProvider from "next-auth/providers/discord";
 
 import { db } from "~/server/db";
 
@@ -69,16 +68,6 @@ export const authConfig = {
         };
       },
     }),
-    DiscordProvider,
-    /**
-     * ...add more providers here.
-     *
-     * Most other providers require a bit more work than the Discord provider. For example, the
-     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
-     * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
-     *
-     * @see https://next-auth.js.org/providers/github
-     */
   ],
   adapter: PrismaAdapter(db),
   callbacks: {
